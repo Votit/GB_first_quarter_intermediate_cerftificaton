1e3311185d7581bed418421c65add07b8842d169
Ivan_Titov <titovid91@gmail.com>
2024-04-22 23:25:26 +0300

    Program completed

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int n = int.Parse(Console.ReadLine());
+    string[] array = new string[n];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите строку № {i + 1}: ");
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("[]");
+    }
+    else
+    {
+        foreach (string str in array)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
+
+string[] CreateNewReducedArray(string[] array)
+{
+    int[] indexesStringLengthLessThreeSymbols = new int[array.Length];
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            indexesStringLengthLessThreeSymbols[count] = i;
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = array[indexesStringLengthLessThreeSymbols[i]];
+    }
+
+    return newArray;
+}
+
+
+string[] arrayOfStrings = CreateArray();
+PrintArray(CreateNewReducedArray(arrayOfStrings));
